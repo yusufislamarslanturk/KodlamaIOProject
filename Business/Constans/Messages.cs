f629ed87ff8bf45bb40d5ff880ccb2de59152335
yusufislamarslanturk <ysarslanturk@gmail.com>
2023-07-17 18:23:42 +0300

    10. Ders sonu

---
+++ Business/Constans/Messages.cs
@@ -0,0 +1,18 @@
+﻿using Entitties.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constans
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+       
+        internal static string ProductsListed="Ürünler listelendi";
+        internal static string MaintenanceTime="Sistem bakımda";
+    }
+}
